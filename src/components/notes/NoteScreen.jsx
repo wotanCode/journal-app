cd6578b407a4dea2c684f361605201678bd69ef9
Pedro Yanez <pedro_y91@outlook.com>
2022-06-10 19:17:59 -0400

    237/estructura y diseño del homescreen

--- src/components/notes/NoteScreen.jsx
+++ src/components/notes/NoteScreen.jsx
@@ -6,6 +6,30 @@ export const NoteScreen = () => {
 
       <NotesAppBar />
 
+      <div className='notes__content'>
+
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete='off'
+        />
+
+        <textarea
+          placeholder="What happened today?"
+          className="notes__textarea"
+        >
+        </textarea>
+
+        <div className='notes__image'>
+          <img
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT57fGQAkDdyP38FrAcvXybaSnwBpMT0gRlHDdnXWWv0v5RvKmMExPRK9pdwrHpYBb-Mgo&usqp=CAU'
+            alt='note'
+          />
+        </div>
+
+      </div>
+
     </div>
   )
 }
