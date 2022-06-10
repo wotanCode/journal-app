6b3133e02ddd24d8688b7730d61c29657eb2dc6b
Pedro Yanez <pedro_y91@outlook.com>
2022-06-10 16:17:33 -0400

    231/estilos loginScreen

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export const LoginScreen = () => {
   return (
     <>
-      <h3>Login</h3>
+      <h3 className='auth__title'>Login</h3>
 
       <form>
 
@@ -9,22 +11,48 @@ export const LoginScreen = () => {
           type='text'
           placeholder='email'
           name='email'
+          className='auth__input'
+          autoComplete='off'
         />
 
         <input
           type='password'
           placeholder='password'
           name='password'
+          className='auth__input'
+          autoComplete='off'
         />
 
         <button
           type='submit'
+          className='btn btn-primary btn-block'
+          disabled={false}
         >
           Login
         </button>
 
-        <hr/>
-        Google
+        <div className='auth__social-networks'>
+
+          <p>Login with social network</p>
+
+          <div
+            className="google-btn"
+          >
+            <div className="google-icon-wrapper">
+              <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+            </div>
+            <p className="btn-text">
+              <b>Sign in with google</b>
+            </p>
+          </div>
+
+        </div>
+
+        <Link
+          to='/auth/register'
+        >
+          Crear nueva cuenta
+        </Link>
 
       </form>
     </>
