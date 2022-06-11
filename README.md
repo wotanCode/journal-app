b85c86c4eb7d8b969dd00cfb30d99df667c7a42c
Pedro Yanez <pedro_y91@outlook.com>
2022-06-10 22:54:44 -0400

    245/configurando redux

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Requisitos
 - node 14.19.2
 - npm 8.9.0
+
+Librerias importantes utilizadas
+- React 18
+- react-redux": "^8.0.2
--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,13 @@
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
 import { AppRouter } from "./routers/AppRouter";
+
 
 export const JournalApp = () => {
   return (
-    <>
+    <Provider store={store}>
       <AppRouter />
-    </>
+    </Provider>
   );
 }
