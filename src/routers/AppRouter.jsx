2e82785d913d54ad684f66741d555dcff24f224f
Pedro Yanez <pedro_y91@outlook.com>
2022-06-10 22:19:40 -0400

    fix

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,23 +2,21 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { AuthRouter } from "./AuthRouter";
 import { JournalScreen } from '../components/journal/JournalScreen';
-import { LoginScreen } from '../components/auth/LoginScreen';
-import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route
-            path="/auth/*"
-            element= {<AuthRouter />}
-          />
+      <Routes>
+        <Route
+          path="/auth/*"
+          element={<AuthRouter />}
+        />
 
-          <Route
-            path="/"
-            element= { <JournalScreen /> }
-          />
-        </Routes>
+        <Route
+          path="/"
+          element={<JournalScreen />}
+        />
+      </Routes>
     </BrowserRouter>
   );
 }
