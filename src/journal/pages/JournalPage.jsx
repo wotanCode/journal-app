b83712bce22a2f7855e372571465374c361a4ec9
Pedro Yanez <pedro_y91@outlook.com>
2022-07-08 03:33:07 -0400

    237 configuracion MUI con vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,13 @@
+import { MailOutline } from "@mui/icons-material";
+import { Typography } from "@mui/material";
+
 export const JournalPage = () => {
   return (
-    <div>
-      <h1>JournalPage</h1>
-    </div>
+    <>
+      <Typography variant='h1'>
+        JournalPage
+      </Typography>
+      <MailOutline />
+    </>
   );
 }
