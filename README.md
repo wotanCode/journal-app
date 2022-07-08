9c983c1b10ace80ca060502eaf54195277d9ae2c
Pedro Yanez <pedro_y91@outlook.com>
2022-07-08 02:26:52 -0400

    NEW VERSION

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 ### Requisitos
 - node 14.19.2
 - npm 8.9.0
-
-Librerias importantes utilizadas
-- React 18
-- react-redux": "^8.0.2
--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,13 +1,7 @@
-import { Provider } from 'react-redux';
-
-import { store } from './store/store';
-import { AppRouter } from "./routers/AppRouter";
-
-
 export const JournalApp = () => {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
   );
 }
