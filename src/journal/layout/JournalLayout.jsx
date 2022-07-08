1077ba4f02ff07c8a057cccae50184ceb13b09d4
Pedro Yanez <pedro_y91@outlook.com>
2022-07-08 07:11:04 -0400

    244 sidebar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
+import { Toolbar } from "@mui/material";
 import { Box } from "@mui/system"
-import { NavBar } from "../components";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -9,12 +10,14 @@ export const JournalLayout = ({ children }) => {
 
       <NavBar drawerWidth={drawerWidth} />
 
-      {/* SideBar drawerWidth */}
+      <SideBar drawerWidth={drawerWidth} />
 
       <Box
         component='main'
         sx={{ flexGrowing: 1, p: 3 }}
       >
+        <Toolbar />
+
         {children}
       </Box>
     </Box>
