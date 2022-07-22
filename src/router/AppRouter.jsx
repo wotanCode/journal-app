ef4d5e787a048871ddd0cbf6b4305e477683ac57
Pedro Yanez <pedro_y91@outlook.com>
2022-07-22 12:17:06 -0400

    287/custum hook

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,31 +1,15 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
-import { FirebaseAuth } from '../firebase/config';
+
+import { useCheckAuth } from '../hooks';
 import { JournalRoutes } from '../journal/routes/JournalRoutes';
 import { CheckingAuth } from '../ui/';
 
 export const AppRouter = () => {
-  const { status } = useSelector(state => state.auth);
-  const dispatch = useDispatch();
 
-
-
-  useEffect(() => {
-    // CheckingAuth();
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout())
-
-      const { uid, email, displayName, photoURL } = user;
-
-      dispatch(login({ uid, email, displayName, photoURL }));
-    });
-  }, []);
-
-
+  const status = useCheckAuth();
 
   if (status === 'checking') {
     return (
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -25,9 +25,9 @@ export const authSlice = createSlice({
       state.email = null;
       state.displayName = null;
       state.photoURL = null;
-      state.errorMessage = payload.errorMessage
+      state.errorMessage = payload?.errorMessage
     },
-    checkingCredentials: (state, payload) => {
+    checkingCredentials: (state) => {
       state.status = 'checking';
     }
   }
